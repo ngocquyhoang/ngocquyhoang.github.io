73ac5dc38ad36d6199e4968f14b65ab8c1b58774
ngocquyhoang <ngocquyhoang.93@gmail.com>
2015-11-12 18:20:27 +0700

    UPDATE COMPLETE EX

--- Javascripts/application.js
+++ Javascripts/application.js
@@ -1,18 +1,17 @@
 $(document).ready(function() {
-	setHeightContainer();
+	setHeight();
 	setHeightHome();
-	setHeightAbout();
+	ExcontentCenter();
+	ExSETHEIGHT();
+	$("body").mCustomScrollbar({theme:"dark-thin",scrollButtons:{enable:true}});
+	$(".experience-box").mCustomScrollbar({theme:"light-thin",scrollButtons:{enable:true}});
 	$('.show-me-button').click(function() {
 		homeShow();
 	});
 });
-function  setHeightContainer () {
+function  setHeight () {
 	var height = $( window ).height();
-	$('.container').height(height);
-}
-function setHeightAbout () {
-	var height = $( window ).height();
-	$('.about').height(height);
+	$('.container, .about, .experience').height(height);
 }
 function setHeightHome () {
 	var height = $( window ).height();
@@ -27,4 +26,12 @@ function homeShow () {
 	$('.details-me .name').delay(3000).show("slide",{direction:'right'}, 1500);
 	$('.details-me .job-1').delay(4500).show("slide",{direction:'right'}, 1500);
 	$('.details-me .job-2').delay(6000).show("slide",{direction:'right'}, 1500);
+}
+function ExcontentCenter () {
+	var width = $(window).width();
+	$('.experience-content').css('left',width/2-500);
+}
+function ExSETHEIGHT () {
+	var height_ex = $('.experience-content').height();
+	$('.experience-box').height(height_ex);
 }
