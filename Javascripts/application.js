df20697ef86ed4665c6040d76dcbe97c0aa15d1d
ngocquyhoang <ngocquyhoang.93@gmail.com>
2015-11-12 13:06:49 +0700

    UPDATE COMPLETE HOME

--- Javascripts/application.js
+++ Javascripts/application.js
@@ -20,6 +20,7 @@ function homeShow () {
 	$('.show-me-button p').hide();
 	$('.show-me-button .avatar').show(1000);
 	$('.details-me').delay(1000).show(500);
+	$('.home .show-me-button').css('background-color','rgba(255,255,255,1.0)');
 	$('.details-me .slat-text').delay(1500).show("slide",{direction:'up'}, 1500);
 	$('.details-me .name').delay(3000).show("slide",{direction:'right'}, 1500);
 	$('.details-me .job-1').delay(4500).show("slide",{direction:'right'}, 1500);
