ce2e20d9e3228aae25d9d7aad6101f1b7eea38e7
Hoang Ngoc Quy <quyhn@nhom51.com>
2016-01-03 02:40:49 +0700

    update code

--- game/league-of-legend/javascript/game-function.js
+++ game/league-of-legend/javascript/game-function.js
@@ -1,21 +1,31 @@
-// on document ready
+/*author @ngocquyhoang*/
 $(document).ready(function(){
-	muteBackgroundAudio();
 });
 // audio control
-function muteBackgroundAudio() {
-	var videoAudioControl = $(".audio-control .mute i");
-	var videoBackground = $('.background-video .tristana-dragon');
-	videoAudioControl.click( function (){
-		// mute video
-	    videoBackground.prop('muted', !videoBackground.prop('muted'));
-	    // change icon
-	    if (videoBackground.prop('muted') == true) {
-			videoAudioControl.removeClass('fa-volume-up');
-			videoAudioControl.addClass('fa-volume-off');
-	    } else{
-	    	videoAudioControl.removeClass('fa-volume-off');
-			videoAudioControl.addClass('fa-volume-up');
-	    };
-	});
+function muteAudio() {
+	var control = $(".control-bar .mute i");
+	var video = $('.background-video .tristana-dragon');
+    video.prop('muted', !video.prop('muted'));
+    if (video.prop('muted') == true) {
+		control.removeClass('fa-volume-up');
+		control.addClass('fa-volume-off');
+    } else{
+    	control.removeClass('fa-volume-off');
+		control.addClass('fa-volume-up');
+    };
 };
+// game control
+function gameControl () {
+	$('div.pause-game').slideDown(500, "linear");
+	control = $(".control-bar .pause i");
+	control.removeClass('fa-pause-circle');
+	control.addClass('fa-play-circle');
+	// game pause
+};
+function resumeGame () {
+	$('div.pause-game').slideUp(500, "linear");
+	control = $(".control-bar .pause i");
+	control.removeClass('fa-play-circle');
+	control.addClass('fa-pause-circle');
+	// resume game
+}
