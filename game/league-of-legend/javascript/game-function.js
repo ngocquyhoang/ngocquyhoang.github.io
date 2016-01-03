0df3cc93e8c7c6ede400d12735ce6857f5b30c8d
Hoang Ngoc Quy <quyhn@nhom51.com>
2016-01-04 00:50:34 +0700

    update code

--- game/league-of-legend/javascript/game-function.js
+++ game/league-of-legend/javascript/game-function.js
@@ -1,8 +1,12 @@
 /*author @ngocquyhoang*/
 $(document).ready(function(){
+	// show select lever...
+	$('.select-lever').delay(1000).slideDown(500, "linear");
+	flipCard();
+	xxxx();
 });
-// audio control
-function muteAudio() {
+// audio control...
+function muteAudio () {
 	var control = $(".control-bar .mute i");
 	var video = $('.background-video .tristana-dragon');
     video.prop('muted', !video.prop('muted'));
@@ -14,18 +18,38 @@ function muteAudio() {
 		control.addClass('fa-volume-up');
     };
 };
-// game control
+// game control...
 function gameControl () {
 	$('div.pause-game').slideDown(500, "linear");
 	control = $(".control-bar .pause i");
 	control.removeClass('fa-pause-circle');
 	control.addClass('fa-play-circle');
-	// game pause
+	// game pause...
 };
 function resumeGame () {
 	$('div.pause-game').slideUp(500, "linear");
 	control = $(".control-bar .pause i");
 	control.removeClass('fa-play-circle');
 	control.addClass('fa-pause-circle');
-	// resume game
+	// resume game...
+}
+function startGame (gameLever) {
+	// hide select game lever...
+	$('.select-lever').slideUp(500, "linear");
+	// body...
+}
+function resetGame () {
+	// reset game...
+	$('div.pause-game').slideUp(500, "linear");
+}
+function mainGame () {
+	
+}
+function flipCard () {
+	$(".card").flip();
+}
+function xxxx () {
+	$('.game-card .game-card-box .card').click(function() {
+	  $(this).addClass("correct");
+	});
 }
